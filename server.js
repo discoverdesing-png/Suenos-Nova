544a9f63e540a58d9bc9a224e6212b795502e68b
Pedro Simental Jacinto <discoverdesing@gmail.com>
2026-05-06 03:29:33 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Sueños-Nova by Pedro Simental Jacinto corriendo en puerto ${PORT}`);
+});
