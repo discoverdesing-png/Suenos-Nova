f362ad0a6b39751048bdc8c91966baffa23d3edd
Pedro Simental Jacinto <discoverdesing@gmail.com>
2026-05-06 04:11:10 -0800

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,1 +1,15 @@
+const mysql = require('mysql2');
+require('dotenv').config();
 
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool.promise();
